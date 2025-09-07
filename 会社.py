b71f290df6237b7c26f67fbf1b9fe093db6a7d7e
kagawayusei <kagawayusei@gmail.com>
2025-09-07 15:14:59 +0900

    昇進システム

--- 会社.py
+++ 会社.py
@@ -2,13 +2,23 @@ import streamlit as st
 
 money=0
 worked=0
+rank=0
 if 'money' not in st.session_state:
     st.session_state.money = money
 elif 'worked' not in st.session_state:
     st.session_state.worked = worked
-st.title("ホーム")
+elif 'rank' not in st.session_state:
+    st.session_state.rank = rank
+st.title("会社")
+st.write(f"現在のランク: {st.session_state.rank}")
 st.write(f"現在の所持金: {st.session_state.money}円")
 if st.button("お金を稼ぐ"):
-    st.session_state.money += 10
+    st.session_state.money += rank+10
     st.session_state.worked += 1
+
+if st.session_state.worked >= 15:
+    st.session_state.rank += 1
+    st.session_state.worked = 0
+    st.balloons()
+    st.success("ランクアップしました！")
 
