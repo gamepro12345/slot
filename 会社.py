57d931ba4cf9595ad9fcfee576ec8a04a240c3fb
kagawayusei <kagawayusei@gmail.com>
2025-09-07 15:29:45 +0900

    ビルが建った

--- 会社.py
+++ 会社.py
@@ -10,14 +10,17 @@ if 'worked' not in st.session_state:
 if 'rank' not in st.session_state:
     st.session_state.rank = rank
 st.title("会社")
-st.write(f"現在のランク: {st.session_state.rank}")
-st.write(f"現在の所持金: {st.session_state.money}円")
+st.header(f"現在のランク: {st.session_state.rank}")
+
+# 画像を表示（例: rank.png）
+st.image("ビル.png")
+
+st.header(f"現在の所持金: {st.session_state.money}円")
 if st.button("お金を稼ぐ"):
-    st.session_state.money += rank+10
+    st.session_state.money += st.session_state.rank+10
     st.session_state.worked += 1
 
 if st.session_state.worked >= 15:
     st.session_state.rank += 1
     st.session_state.worked = 0
     st.success("ランクアップしました！")
-
