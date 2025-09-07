7214fa7fb8e29f4b83dc0ce84c31663dbd9862ec
kagawayusei <kagawayusei@gmail.com>
2025-09-07 15:10:16 +0900

    p2

--- 会社.py
+++ 会社.py
@@ -1,12 +1,14 @@
 import streamlit as st
 
 money=0
-
+worked=0
 if 'money' not in st.session_state:
     st.session_state.money = money
-
+elif 'worked' not in st.session_state:
+    st.session_state.worked = worked
 st.title("ホーム")
 st.write(f"現在の所持金: {st.session_state.money}円")
 if st.button("お金を稼ぐ"):
     st.session_state.money += 10
+    st.session_state.worked += 1
 
